1110dfb4356939db8aa370bce92d41ea9bd1fdf2
Zoha <zoha@intelligentmachin.es>
2022-01-26 13:46:54 +0600

    added tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
 }

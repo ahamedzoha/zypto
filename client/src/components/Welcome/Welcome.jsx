cb5e6da82a0aaa52c374c88b49af96b8afb3c05f
Zoha <zoha@intelligentmachin.es>
2022-02-13 14:01:13 +0600

    layout issues

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -15,7 +15,7 @@ const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col mf:mr-10">
+        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
@@ -30,6 +30,7 @@ const Welcome = () => {
             bg-[#2952e3] p-3 rounded-full text-white text-base 
             font-semibold cursor-pointer hover:bg-[#2546bd]"
           >
+            <AiFillPlayCircle className="text-white mr-2" />
             Connect Wallet
           </button>
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
@@ -42,10 +43,11 @@ const Welcome = () => {
           </div>
         </div>
       </div>
+
       <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-        <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+        <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
           <div className="flex justify-between flex-col w-full h-full">
-            <div className="flex justify-between items-start ">
+            <div className="flex justify-between items-start">
               <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
                 <SiEthereum fontSize={21} color="#fff" />
               </div>

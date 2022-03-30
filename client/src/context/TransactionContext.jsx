56e4fce335319aab0d423e04a7692ba650da32cd
ahamedzoha <ahamed.zoha@gmail.com>
2022-03-30 19:05:58 +0600

    added TODO

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react"
 import { ethers } from "ethers"
 import { contractAbi, contractAddress } from "../utils/constants"
 
+// TODO: Understand how to use the contract abi and address
+
 export const TransactionContext = React.createContext()
 
 const { ethereum } = window
